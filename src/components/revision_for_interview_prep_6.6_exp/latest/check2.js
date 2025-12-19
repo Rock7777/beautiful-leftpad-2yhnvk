588de85707ec04285b2d29e6adc6e3815679b591
Rock7777 <61678762+Rock7777@users.noreply.github.com>
2025-12-19 23:02:31 +0530

    Create check2.js
    VS code learnings

---
+++ src/components/revision_for_interview_prep_6.6_exp/latest/check2.js
@@ -0,0 +1,23 @@
+#VS Code learnings
+
+Making better use of VS code:
+
+shorcuts:
+
+Ctrl+P --> recently open files or search for files - I know it
+
+I can use Quokka.js extension which gives out output of JS code in the code itself --> will try it in my personal app
+
+To navigate through the entire file for getting entire content of file like functions and variable used --> use @ after using Ctrl + P 
+and then u can find out --> this is better than Ctrl + F and direct shortcut is Ctrl+Shift+.
+
+to go to certain use Ctrl+G followed by line number
+
+to edit multiple occurences at a time use Ctrl + D and the n start typing
+
+to automatically get the closing tag for opening tag use extension Auto Rename Tag
+
+to move line of code upper or lower use Alt + top/down arrow keys
+to copy and move tat line of code upper or lower use Alt + Shift + top/down arrow keys
+
+Open a new terminal -> Ctrl + ` , to clear out terminal use Ctrl + K
